d041af161782901bafff87b4b284a807e5d4bbad
federicofrontera <federicofrontera@gmail.com>
2020-04-16 13:36:29 -0300

    forcing circleci

--- src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
+++ src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class IngredientToIngredientCommand implements Converter<Ingredient, IngredientCommand> {
-
     private final UnitOfMeasureToUnitOfMeasureCommand uomConverter;
 
     public IngredientToIngredientCommand(UnitOfMeasureToUnitOfMeasureCommand uomConverter) {
@@ -29,9 +28,7 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
 
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
-        if (ingredient.getRecipe() != null) {
-            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
-        }
+
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
